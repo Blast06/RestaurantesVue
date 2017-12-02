d3b76378600ad1b1b4ee0fed15fb2da742ae9510
Blast06 <lacalculadora06@hotmail.com>
2017-12-01 21:02:13 -0430

    Agregado carga de restaurantes utilizando api rest del curso de vue.

--- src/restaurantesList.vue
+++ src/restaurantesList.vue
@@ -9,11 +9,25 @@
 
 
 <script>
+  import axios from 'axios';
   export default {
     name: 'restauranteslist',
+    mounted(){
+        this.getRestaurants();
+
+    },
     data(){
       return{
-        texto:'Pagina Restaurantes LIST'
+        texto:'Pagina Restaurantes LIST',
+        restaurantes: null
+      }
+    },
+    methods:{
+      getRestaurants(){
+        axios.get('http://localhost/slim/restaurantes-api.php/restaurantes')
+          .then((respuesta) => {
+            this.restaurantes = respuesta.data.data;
+          });
       }
     }
   }
